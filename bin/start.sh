9b03cab960102b953c026175ce87afd5126243a0
jiantao01 <jian.tao@afterpay.com>
2021-09-11 23:17:54 +0800

    add pid

--- bin/start.sh
+++ bin/start.sh
@@ -2,4 +2,4 @@ nohup gunicorn -w 2 \
       -k gevent   \
       --access-logfile '-' \
       --error-logfile '-' \
-      -b 0.0.0.0:80 manage:app >> logs/std.log 2>&1 & echo $! >> logs/pid
+      -b 0.0.0.0:80 manage:app >> logs/std.log 2>&1 & echo $! > logs/pid
