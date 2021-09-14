c297f8b9b2b7d361796079a6aab5b94f54c10c44
jiantao01 <jian.tao@afterpay.com>
2021-09-14 16:00:19 +0800

    update

--- server/utils.py
+++ server/utils.py
@@ -1,11 +1,44 @@
 # -*- coding:utf-8 -*-
-import parsedatetime
 from time import mktime
-from datetime import datetime
+import config
 import json
 import copy
 import hashlib
 import requests
+import email
+import imaplib
+import re
+import parsedatetime
+from datetime import datetime
+from dateutil.parser import parse
+from flask import Flask, jsonify, render_template, request
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def dttm_from_timtuple(d):
+    return datetime(
+        d.tm_year, d.tm_mon, d.tm_mday, d.tm_hour, d.tm_min, d.tm_sec)
+
+
+def parse_datetime(s):
+    if not s:
+        return None
+    try:
+        dttm = parse(s)
+    except Exception:
+        try:
+            cal = parsedatetime.Calendar()
+            parsed_dttm, parsed_flags = cal.parseDT(s)
+            # when time is not extracted, we 'reset to midnight'
+            if parsed_flags & 2 == 0:
+                parsed_dttm = parsed_dttm.replace(hour=0, minute=0, second=0)
+            dttm = dttm_from_timtuple(parsed_dttm.utctimetuple())
+        except Exception as e:
+            raise ValueError("Couldn't parse date string [{}]".format(s))
+    return dttm
 
 
 def request_loader(request):
@@ -31,8 +64,47 @@ def parse_human_time(s):
 def fetch_code(order, host):
     membership = order.membership
     params = membership.email_config()
-    res = requests.get(host, params=params)
-    return res.json()
+    EMAIL = params.get("email")
+    PASSWORD = params.get("password")
+    try:
+        SERVER = 'imap.gmail.com'
+        mail = imaplib.IMAP4_SSL(SERVER)
+        mail.login(EMAIL, PASSWORD)
+        mail.select('inbox')
+        status, data = mail.search(None, 'ALL')
+        mail_ids = []
+
+        for block in data:
+            mail_ids += block.split()
+        final_mail = None
+        for i in mail_ids:
+            status, data = mail.fetch(i, '(RFC822)')
+            mail_content = ''
+            for response_part in data:
+                if isinstance(response_part, tuple):
+                    message = email.message_from_bytes(response_part[1])
+
+                    mail_from = message['from']
+                    if mail_from != "Educative <support@educative.io>":
+                        continue
+                    mail_subject = message['subject']
+                    if message.is_multipart():
+                        mail_content = ''
+                        for part in message.get_payload():
+                            if part.get_content_type() == 'text/plain':
+                                mail_content += part.get_payload()
+                    else:
+                        mail_content = message.get_payload()
+                    final_mail = mail_content
+
+        code = re.findall("Educative: ([0-9]{6})", final_mail)
+        if code:
+            msg = code[0]
+        else:
+            msg = '0'
+        return jsonify(code=msg)
+    except Exception as e:
+        return jsonify(code='0', error=str(e))
 
 
 def parse_location(ip):
