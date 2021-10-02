cd96148ebf308eddfe496988a7c7d52564dff003
jiantao01 <jian.tao@afterpay.com>
2021-10-02 13:44:27 +0800

    add build

---
+++ webapp/build/precache-manifest.c608f98b7c3595340745b7c952ca7f30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1108504942cf29794c5e6f113ec12f1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fa5e95444f8c527d7d9",
+    "url": "/static/css/2.0e8ca5e5.chunk.css"
+  },
+  {
+    "revision": "306819c7ca80cd6800f4",
+    "url": "/static/css/main.8575e1ae.chunk.css"
+  },
+  {
+    "revision": "9fa5e95444f8c527d7d9",
+    "url": "/static/js/2.05b449d3.chunk.js"
+  },
+  {
+    "revision": "7fc3b5f809ede72074aafbf26669f1c3",
+    "url": "/static/js/2.05b449d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306819c7ca80cd6800f4",
+    "url": "/static/js/main.e908b24d.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
