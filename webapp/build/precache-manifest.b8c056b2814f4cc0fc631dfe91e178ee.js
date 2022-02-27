3d50b70ab7ef5da7aaf035511b9f3bc4fd90afdd
taojian566 <taojian566@gmail.com>
2022-02-27 17:06:59 +0800

    add front end js

---
+++ webapp/build/precache-manifest.b8c056b2814f4cc0fc631dfe91e178ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c05e49492f9ee1445e24fbccf650d7c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "250fa4b3cb6931674718",
+    "url": "/static/css/2.0e8ca5e5.chunk.css"
+  },
+  {
+    "revision": "a9d5e17eee0f53ae934d",
+    "url": "/static/css/main.8575e1ae.chunk.css"
+  },
+  {
+    "revision": "250fa4b3cb6931674718",
+    "url": "/static/js/2.2bfab698.chunk.js"
+  },
+  {
+    "revision": "7fc3b5f809ede72074aafbf26669f1c3",
+    "url": "/static/js/2.2bfab698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9d5e17eee0f53ae934d",
+    "url": "/static/js/main.a3e689c3.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
